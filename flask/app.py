8051d0a70b7b3aa4d2da78c617413e33d5b69ef0
Chmonya-stalker <dlastima2001@gmail.com>
2023-06-04 01:19:48 +0300

    добавил необходимое в компосе и поменял приложения фласка

--- flask/app.py
+++ flask/app.py
@@ -5,47 +5,69 @@ from flask import request
 import emoji
 app = Flask(__name__)
 
-#Создание маршрута для метода POST
-@app.route('/', methods=['POST'])
-def post_metod():
+# Функция для проверок всех условий для взодящих значений
+def check_and_output(animal,sound,count):
 
-    # Работа с переменной 'animal'
-    animal = request.form.get('animal')
-    if animal is None:
-        return 'You have not passed the required animal key'
-    # Проверка поля "Животное"
     if len(animal) <= 0 or animal.isspace():
-       return 'Invalid animal name format'
+        return 'Invalid animal name format'
     else:
-        animal = animal.strip()
+            animal = animal.strip().lower()
 
-    # Работа с переменной 'count'
-    count = request.form.get('count')
-    if count is None:
-        return 'You have not passed the required count key'
     if len(count) <= 0 or count.isspace():
-        return 'Count is empty.'
+        return 'Wrong number format'
     else:
         try: 
             count = int(count)
             if count < 1:
-                return 'Count < 1'
+                return 'The number cannot be less than 1'
         except:
             return 'Invalid Count format.'
-           
-    # Формирование звуков
-    sound = request.form.get('sound')
-    for i in range(count-1):
-        sound +=" " + sound
+
+    if len(sound) <= 0 or sound.isspace():
+        return 'Invalid sound format'
+    sound =(sound + " ") * count
+
+    # Проверка существования животного и эмоджи #
+    emoji_animal = emoji.emojize(f':{animal}:')
+    if emoji_animal == f':{animal}:':
+        return 'There is no such animal. Check the title'
 
     return '''
-            <h1>Привет! Я {} {}!</h1>
-            <h1>Я делаю {}</h1>
-            '''.format(animal, emoji.emojize(f':{animal}:'), sound)
+        <h1>Привет! Я {} {}!</h1>
+        <h1>Я делаю {}</h1>
+        '''.format(animal, emoji_animal, sound)
 
-#Создание маршрута для метода GET
+# Создание маршрута для JSON #
+@app.route('/json/', methods=['POST'])
+def json_metod():
+
+    request_data = request.get_json()
+
+    if ('animal' in request_data) and ('sound' in request_data) and ('count' in request_data):
+        animal = request_data['animal']
+        sound = request_data['sound']
+        count = request_data['count']
+
+    else:
+        return 'Одно из обязательных полей пропущено!'
+
+    return check_and_output(animal,sound,count)
+
+
+# Создание маршрута для метода POST #
+@app.route('/', methods=['POST'])
+def post_metod():
+
+    animal = request.form.get('animal')
+    sound = request.form.get('sound')
+    count = request.form.get('count')
+    if (animal is None) or (sound is None) or (count is None):
+        return 'Одно из обязательных полей пропущено!'
+
+    return check_and_output(animal,sound,count)
+
+# Создание маршрута для метода GET #
 @app.route('/', methods=['GET'])
-# Добавь маршрут для метода POST, что бы принимал json формата: {animal: foo, count: number, sound: bar}
 def get_metod():
     return '''
       <form method="POST">
@@ -55,7 +77,7 @@ def get_metod():
             <div><label>Какой вы хотите услышать звук от животного: <input type="text" name="sound"></label></div>
             <div><label>Сколько раз вы услышать его звук: <input type="number" name="count"></label></div>
             <input type="submit" value="К животному">
-проверка как работает
         </form>'''
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
